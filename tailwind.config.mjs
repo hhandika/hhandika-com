3f0d8f09cabb15b0b3d1eaa70dea2520ba688f06
Heru Handika <herubiolog@gmail.com>
2024-11-07 14:02:53 -0600

    Update colors.

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {},
+    colors: {
+      "talamau-blue": "#1B578C",
+      "talamau-light-blue": "#537FA6",
+      "talamau-yellw": "#F2BF27",
+      "talamau-brown": "#A66414",
+      "talamau-dark-brown": "#59290C",
+    },
+  },
+  plugins: [],
+};
