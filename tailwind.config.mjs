76f7ef2ff31c0b6ff5075ebf90823c03295d55dd
Heru Handika <herubiolog@gmail.com>
2024-11-07 14:03:27 -0600

    Extend colors.

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,13 +2,14 @@
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
-    colors: {
-      "talamau-blue": "#1B578C",
-      "talamau-light-blue": "#537FA6",
-      "talamau-yellw": "#F2BF27",
-      "talamau-brown": "#A66414",
-      "talamau-dark-brown": "#59290C",
+    extend: {
+      colors: {
+        "talamau-blue": "#1B578C",
+        "talamau-light-blue": "#537FA6",
+        "talamau-yellw": "#F2BF27",
+        "talamau-brown": "#A66414",
+        "talamau-dark-brown": "#59290C",
+      },
     },
   },
   plugins: [],
